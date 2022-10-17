f70af9dc7619cdd1aee728441700ebccf7b0608c
LouieAlcantara <luna.alcantara19@gmail.com>
2022-10-16 21:04:08 -0300

    primeira aula

---
+++ primeiro_arquivo.py
@@ -0,0 +1,11 @@
+print('ola mundo')
+print('brasil', 'ganhou', '5 titulos mundiais', sep='-')
+print('AI, QUE, DELICIA', end=(' ui\n'))
+
+pais = 'italia'
+quantidade = 4
+
+print(pais,'ganhou',quantidade,'titulos mundiais')
+
+type(pais)
+
