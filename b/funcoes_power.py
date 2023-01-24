57b58ff477e97501cd7272b57fc3f30b175146f3
LouieAlcantara <luna.alcantara19@gmail.com>
2023-01-23 21:12:50 -0300

    e

---
+++ b/funcoes_power.py
@@ -0,0 +1,11 @@
+#função lambda
+multiplicar = lambda x : x*2
+print(multiplicar(9))
+
+absoluto = lambda x , y : abs(x - y) < 3
+print(absoluto(2,4))
+
+operaçoes = lambda x , n : x*n+x/n
+print(operaçoes(5,8))
+
+#funçao map
